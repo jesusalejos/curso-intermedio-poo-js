8924e3a1beb46aa1b8f0df8fd37ad2eec3b10f1f
Jesús Alejos <jalejos6@gmail.com>
2022-06-06 08:46:43 -0430

    First module

--- main.js
+++ main.js
@@ -1,81 +1,81 @@
-function isObject(subject) {
-  return typeof subject == "object";
-}
+// function isObject(subject) {
+//   return typeof subject == "object";
+// }
 
-function isArray(subject) {
-  return Array.isArray(subject);
-}
+// function isArray(subject) {
+//   return Array.isArray(subject);
+// }
 
-function deepCopy(subject) {
-  let copySubject;
+// function deepCopy(subject) {
+//   let copySubject;
 
-  const subjectIsObject = isObject(subject);
-  const subjectIsArray = isArray(subject);
+//   const subjectIsObject = isObject(subject);
+//   const subjectIsArray = isArray(subject);
 
-  if (subjectIsArray) {
-    copySubject = [];
-  } else if (subjectIsObject) {
-    copySubject = {};
-  } else {
-    return subject;
-  }
+//   if (subjectIsArray) {
+//     copySubject = [];
+//   } else if (subjectIsObject) {
+//     copySubject = {};
+//   } else {
+//     return subject;
+//   }
 
-  for (key in subject) {
-    const keyIsObject = isObject(subject[key]);
+//   for (key in subject) {
+//     const keyIsObject = isObject(subject[key]);
 
-    if (keyIsObject) {
-      copySubject[key] = deepCopy(subject[key]);
-    } else {
-      if (subjectIsArray) {
-        copySubject.push(subject[key]);
-      } else {
-        copySubject[key] = subject[key];
-      }
-    }
-  }
+//     if (keyIsObject) {
+//       copySubject[key] = deepCopy(subject[key]);
+//     } else {
+//       if (subjectIsArray) {
+//         copySubject.push(subject[key]);
+//       } else {
+//         copySubject[key] = subject[key];
+//       }
+//     }
+//   }
 
-  return copySubject;
-}
+//   return copySubject;
+// }
 
 
-function SuperObject() {}
-SuperObject.isObject = function (subject) {
-  return typeof subject == "object";
-}
-SuperObject.deepCopy = function (subject) {
-  let copySubject;
+// function SuperObject() {}
+// SuperObject.isObject = function (subject) {
+//   return typeof subject == "object";
+// }
+// SuperObject.deepCopy = function (subject) {
+//   let copySubject;
 
-  const subjectIsObject = isObject(subject);
-  const subjectIsArray = isArray(subject);
+//   const subjectIsObject = isObject(subject);
+//   const subjectIsArray = isArray(subject);
 
-  if (subjectIsArray) {
-    copySubject = [];
-  } else if (subjectIsObject) {
-    copySubject = {};
-  } else {
-    return subject;
-  }
+//   if (subjectIsArray) {
+//     copySubject = [];
+//   } else if (subjectIsObject) {
+//     copySubject = {};
+//   } else {
+//     return subject;
+//   }
 
-  for (key in subject) {
-    const keyIsObject = isObject(subject[key]);
+//   for (key in subject) {
+//     const keyIsObject = isObject(subject[key]);
 
-    if (keyIsObject) {
-      copySubject[key] = deepCopy(subject[key]);
-    } else {
-      if (subjectIsArray) {
-        copySubject.push(subject[key]);
-      } else {
-        copySubject[key] = subject[key];
-      }
-    }
-  }
+//     if (keyIsObject) {
+//       copySubject[key] = deepCopy(subject[key]);
+//     } else {
+//       if (subjectIsArray) {
+//         copySubject.push(subject[key]);
+//       } else {
+//         copySubject[key] = subject[key];
+//       }
+//     }
+//   }
 
-  return copySubject;
-}
+//   return copySubject;
+// }
 
-function requiredParam(param) {
-  throw new Error(param + " es obligatorio");
-}
+// function requiredParam(param) {
+//   throw new Error(param + " es obligatorio");
+// }
 
 function LearningPath({
   name = requiredParam("name"),
@@ -129,6 +129,9 @@ function Student({
 
 const escuelaWeb = new LearningPath({ name: "Escuela de WebDev" });
 const escuelaData = new LearningPath({ name: "Escuela de Data Science" });
+
+//Class 3
+
 const juan = new Student({
   email: "juanito@frijoles.co",
   name: "Juanito",
@@ -137,3 +140,45 @@ const juan = new Student({
     escuelaData,
   ],
 });
+
+
+// console.log(Object.keys(juan));
+// console.log(Object.getOwnPropertyNames(juan));
+// console.log(Object.values(juan));
+// console.log(Object.entries(juan));
+// console.log(Object.getOwnPropertyDescriptors(juan));
+
+//Class 4
+
+Object.seal(juan);
+console.log(Object.getOwnPropertyDescriptors(juan));
+
+Object.defineProperty(juan, "navigator", {
+    value: "Chrome",
+    enumerable: false,
+    writable: true,
+    configurable: true,
+});
+
+Object.defineProperty(juan, "editor", {
+    value: "VSCode",
+    enumerable: true,
+    writable: false,
+    configurable: true
+});
+
+Object.defineProperty(juan, "terminal", {
+    value: "WSL",
+    enumerable: true,
+    writable: true,
+    configurable: false,
+});
+
+Object.defineProperty(juan, "pruebNasa", {
+    value: "marcianito",
+    enumerable: false,
+    writable: false,
+    configurable: false,
+});
+
+console.log(Object.getOwnPropertyDescriptors(juan));
